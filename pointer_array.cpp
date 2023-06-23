26fe5b6910d3f9e5057c995a1e67fec0a0131a30
Oleksandr <fedorut20@gmail.com>
2023-06-23 14:42:09 +0300

    ex_1 task done

--- pointer_array.cpp
+++ pointer_array.cpp
@@ -1,36 +1,26 @@
-// pointer_array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <ctime>
 using namespace std;
 
-int main()
-{
-    srand(time(0));
-    int size;
-    cout << "Enter size of array: ";
-    cin >> size;
-    int* arr = new int[size];
-    for (size_t i = 0; i < size; i++)
-    {
-        *(arr + i) = rand() % 10;
-        cout << *(arr + i) << " ";
+int main() {
+    const int SIZE = 5;
+    int* arr1 = new int[SIZE] {1, 2, 3, 4, 5};
+    int* arr2 = new int[SIZE];
+
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
+
+    for (int i = 0; i < SIZE; i++) {
+        *(ptr2 + i) = *(ptr1 + i);
     }
 
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr2[i] << " ";
+    }
     cout << endl;
-    cout << *(arr + (size - 1)) << endl;
-    delete[] arr;
+
+    delete[] arr1;
+    delete[] arr2;
+
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
