11ff8ca6b1c1002581291e3099dea1a580f999f6
Oleksandr <fedorut20@gmail.com>
2023-06-23 14:48:44 +0300

    ex_2 task done

--- pointer_array.cpp
+++ pointer_array.cpp
@@ -1,36 +1,39 @@
 // pointer_array.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-int main()
-{
-    srand(time(0));
-    int size;
-    cout << "Enter size of array: ";
-    cin >> size;
-    int* arr = new int[size];
-    for (size_t i = 0; i < size; i++)
-    {
-        *(arr + i) = rand() % 10;
-        cout << *(arr + i) << " ";
-    }
+void rev_arr(int* arr, int size) {
+    int* start = arr;                  
+    int* end = arr + size - 1;         
 
-    cout << endl;
-    cout << *(arr + (size - 1)) << endl;
-    delete[] arr;
-    return 0;
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    const int SIZE = 5;
+    int numbers[SIZE] = { 1, 2, 3, 4, 5 };
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    cout << "Original Array: ";
+    for (int i = 0; i < SIZE; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
+    rev_arr(numbers, SIZE);
+
+    cout << "Reversed Array: ";
+    for (int i = 0; i < SIZE; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
